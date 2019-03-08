ec44b14c07e83847d24cab19d1e9b0c480b658d2
unknown <malinebbesen@gmail.com>
2019-03-08 15:38:04 +0100

    malins

--- GitExercise/Program.cs
+++ GitExercise/Program.cs
@@ -7,6 +7,8 @@ namespace GitExercise
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Malins ändring");
         }
     }
 }
