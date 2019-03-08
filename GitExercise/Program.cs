3c3bb17d39173ac0f562e9eecaa2ea582a9ab5d5
frwa7342 <fredrik.waern@hotmail.com>
2019-03-08 15:41:33 +0100

    hej hej

--- GitExercise/Program.cs
+++ GitExercise/Program.cs
@@ -6,7 +6,7 @@ namespace GitExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I love GIT!");
         }
     }
 }
